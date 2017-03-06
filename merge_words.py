fe9cc46bb1fe67060bc1386170e977ef1371c34a
joshualeung <lcm.hust@gmail.com>
2017-03-06 20:36:43 +0800

    script refactor.

--- merge_words.py
+++ merge_words.py
@@ -5,10 +5,15 @@ import os
 
 def main():
     base_dir = "download/"
+    target_file = "data/words.dic"
     files = [
         "baidu.txt", "baidu-hotword.txt", "star.txt"
     ]
     words = set([])
+    with open(target_file) as fi:
+        for line in fi:
+            words.add(line.strip())
+        fi.close()
     for f in files:
         f = base_dir + f
         with open(f) as fi:
@@ -16,11 +21,13 @@ def main():
                 line = line.strip().lower()
                 cols = line.split("\t")
                 words.add(cols[0])
-    with open("data/words.dic", "w") as fo:
+            fi.close()
+    with open(target_file, "w") as fo:
         words_list = list(words)
         words_list = sorted(words_list)
         for word in words_list:
             fo.write(word + "\n")
+        fo.close()
 
 if __name__ == '__main__':
     main()
